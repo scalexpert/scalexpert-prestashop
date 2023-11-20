30864b4347c05f32cbdd8221c96c15259233abf9
Sébastien FRANCHON <sfranchon@datasolution.fr>
2023-11-20 16:53:58 +0100

    Update UATC url

--- prestashop-v1.6-1.7/scalexpertplugin/src/Api/Client.php
+++ prestashop-v1.6-1.7/scalexpertplugin/src/Api/Client.php
@@ -118,7 +118,7 @@ class Client
         if ('PRODUCTION' === strtoupper($this->appEnvironnement)) {
             $apiURL = 'https://api.scalexpert.societegenerale.com/baas/prod/';
         } else {
-            $apiURL = 'https://api.scalexpert.uatc.societegenerale.com/baas/uatc';
+            $apiURL = 'https://api.scalexpert.uatc.societegenerale.com/baas/uatc/';
         }
 
         return $apiURL;
